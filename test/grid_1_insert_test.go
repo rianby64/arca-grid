90b522a2daea790d84ecfd18d90f4f3a7fe180f3
Andres Rios <rianby64@gmail.com>
2018-12-07 19:09:05 +0300

    Test result from insert and notify

--- test/grid_1_insert_test.go
+++ test/grid_1_insert_test.go
@@ -140,3 +140,53 @@ func Test_result_from_insert(t *testing.T) {
 		t.Error("result message differs from the expected")
 	}
 }
+
+func Test_result_from_insert_and_notify(t *testing.T) {
+	t.Log("Test result from insert")
+
+	// Setup
+	var msgActual string
+	msgExpected := "a complex result"
+
+	var listener src.NotifyCallback = func(message interface{}) {
+
+		// Verify
+		if message == nil {
+			t.Error("received message is nil")
+		}
+		if message.(string) != msgExpected {
+			t.Error("received message differs from the expected")
+		}
+		msgActual = message.(string)
+	}
+
+	var insertDefinition src.RequestHandler = func(
+		requestParams *interface{},
+		context *interface{},
+		notify src.NotifyCallback,
+	) (interface{}, error) {
+
+		// Excercise
+		notify(msgExpected)
+		return msgExpected, nil
+	}
+
+	server := src.Grid{}
+	server.Listen(&listener)
+	server.RegisterMethod("insert", &insertDefinition)
+
+	// Excercise
+	resultActual, err := server.Insert(nil, nil)
+	if err != nil {
+		t.Error("Unexpected error")
+	}
+	if resultActual == nil {
+		t.Error("Action server.insert returned nil")
+	}
+	if resultActual != msgExpected {
+		t.Error("result message differs from the expected")
+	}
+	if msgActual != msgExpected {
+		t.Error("received message differs from the expected")
+	}
+}
