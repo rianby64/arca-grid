c3aba50d1fd5c8effcc7f8d1c21ec85bc2768c60
Andres Rios <rianby64@gmail.com>
2018-12-07 18:45:02 +0300

    Add test to the insert action

--- test/grid_1_insert_test.go
+++ test/grid_1_insert_test.go
@@ -48,3 +48,92 @@ func Test_Notify_from_insertDefinition(t *testing.T) {
 		t.Error("received message differs from the expected")
 	}
 }
+
+func Test_Notifications_from_insertDefinition(t *testing.T) {
+	t.Log("Test notifications from insertDefinition")
+
+	// Setup
+	var msgActual1 string
+	var msgActual2 string
+	msgExpected := "message expected"
+
+	var listener1 src.NotifyCallback = func(message interface{}) {
+
+		// verify
+		if message == nil {
+			t.Error("received message is nil")
+		}
+		if message.(string) != msgExpected {
+			t.Error("received message differs from the expected")
+		}
+		msgActual1 = message.(string)
+	}
+
+	var listener2 src.NotifyCallback = func(message interface{}) {
+
+		// verify
+		if message == nil {
+			t.Error("received message is nil")
+		}
+		if message.(string) != msgExpected {
+			t.Error("received message differs from the expected")
+		}
+		msgActual2 = message.(string)
+	}
+
+	var insertDefinition src.RequestHandler = func(
+		requestParams *interface{},
+		context *interface{},
+		notify src.NotifyCallback,
+	) (interface{}, error) {
+
+		// excercise
+		notify(msgExpected)
+		return nil, nil
+	}
+
+	server := src.Grid{}
+	server.Listen(&listener1)
+	server.Listen(&listener2)
+	server.RegisterMethod("insert", &insertDefinition)
+
+	// Excercise
+	server.Insert(nil, nil)
+
+	if msgActual1 != msgExpected {
+		t.Error("received message differs from the expected")
+	}
+
+	if msgActual2 != msgExpected {
+		t.Error("received message differs from the expected")
+	}
+}
+
+func Test_result_from_insert(t *testing.T) {
+	t.Log("Test result from insert")
+
+	// Setup
+	resultExpected := []string{"a complex result"}
+
+	var insertDefinition src.RequestHandler = func(
+		requestParams *interface{},
+		context *interface{},
+		notify src.NotifyCallback,
+	) (interface{}, error) {
+
+		// excercise
+		return resultExpected, nil
+	}
+
+	server := src.Grid{}
+	server.RegisterMethod("insert", &insertDefinition)
+
+	// Excercise
+	resultActual, err := server.Insert(nil, nil)
+	if err != nil {
+		t.Error("Unexpected error")
+	}
+	if resultActual == nil {
+		t.Error("Action server.insert returned nil")
+	}
+}
